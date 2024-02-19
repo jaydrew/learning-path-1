eb1ff42541082b184066076911f4015a0da96d65
jaydrew <jaydrew2716@gmail.com>
2024-02-19 12:06:31 +0630

    first try

---
+++ 4-LEARN-JS/basic/learn.js
@@ -0,0 +1,117 @@
+//console.log ("Hello I'm learning Javascript")
+
+
+//function findAge(birthYear){
+   // let currentYear = 2024;
+  //  let age = currentYear - birthYear
+   // return age;
+   // }
+    
+   // console.log(findAge(1994))
+   // console.log(findAge(2005))
+//    console.log(findAge(2002))
+
+   // buying product
+   // 1. select product
+   // 2. check price
+   // 3. quantity
+   // cost = price * quantity
+
+
+   // tax (percentage)
+   // 30000 amount , tax 5% = ?
+   // 500000 amount , tax 5% = ?
+   // 7000 amount , tax 5% = ?
+
+
+
+//    let total = 0;
+
+//    function buy(productName,productPrice,quantity){
+//     let cost = productPrice * quantity;
+//     total += cost;
+//     return productName+" - "+quantity+" - "+cost;
+//    }
+
+//    function findTax(amount,percentage){
+//     return amount * (percentage/100);
+//    }
+
+//    console.log("Shopping Receipt");
+
+//    console.log(findTax(30000,5));
+//    console.log(findTax(500000,5));
+//    console.log(findTax(7000,5));
+
+//    console.log(buy("apple",500,5));
+//    console.log(buy("orange",400,5));
+//    console.log(buy("mango",1500,5));
+//    console.log(buy("strawberries",1200,8));
+// console.log("-------------------------")
+
+//    console.log("total -"+total)
+//    let tax = findTax(total,5);
+//    console.log("tax - "+tax);
+
+//    console.log("------------------");
+   
+//    let netTotal = tax + total;
+//    console.log("cash - " + netTotal);
+    
+//    if(netTotal > 10000){
+//     console.log("U got lucky draw");
+//    }
+
+//isEven or isOdd
+
+// number/2
+// if reminder(%) is 1 = odd
+// else reminder is 0 = even
+
+// conditional statement
+
+// function findEvenOdd(number){
+//     let reminder = number%2
+//    if(reminder == 1){
+//     return "Odd";
+//    }else{
+//     return "Even"
+//    }
+// }
+
+// console.log(findEvenOdd(7));
+// console.log(findEvenOdd(14));
+// console.log(findEvenOdd(30));
+// console.log(findEvenOdd(1995));
+
+
+// function findPassFail(mark) {
+//    if (mark >= 40){
+//       return "passed";
+//    } else {
+//       return "failed";
+//    }
+// }
+
+// console.log(findPassFail(50));
+// console.log(findPassFail(45));
+// console.log(findPassFail(20));
+// console.log(findPassFail(70));
+// console.log(findPassFail(56));
+// console.log(findPassFail(35));
+
+// let marks = [50,45,70,79,53,85];
+// let markObi = {
+//    mm : 50,
+//    en : 45,
+//    math : 70,
+//    chem: 79,
+//    phy: 52,
+//    bio: 85,
+// };
+ 
+for (let i = 100; i >= 1; i-=6){
+   console.log(i);
+}
+
+
---
+++ test.md
@@ -0,0 +1,16 @@
+# Min ga lar par
+san kyi tar par
+
+
+* apple
+* orange
+* mango
+
+## Text Formatting
+ _ * Italic text * or _Italic Text_
+ _ **Bold Text** or __Bold Text__
+ _ `Inline Code`
+
+ ## Links and Images
+ [link to Google](https://www.google.com)
+![Image Alt Text](./assets/images/poe-mamhe-thar-3.jpeg)
